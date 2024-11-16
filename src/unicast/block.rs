e505da3c5dec7154b1d5c5d02090911e1be677fb
tower120 <tower120@gmail.com>
2024-11-16 04:41:17 +0200

    unicast Block now have separate Header and Memory.
    This allows to split Memory deallocation from block. Reader that holds
    Arc<Block> now does not prevent it's memory from being deallocated.

--- src/unicast/block.rs
+++ src/unicast/block.rs
@@ -1,10 +1,14 @@
 use std::cell::UnsafeCell;
+use std::hint::unreachable_unchecked;
+use std::{mem, ptr};
 use std::mem::MaybeUninit;
+use std::ptr::NonNull;
 use std::sync::Arc;
-use std::sync::atomic::{AtomicBool, AtomicU64, AtomicUsize};
+use std::sync::atomic::{AtomicBool, AtomicPtr, AtomicU64, AtomicUsize, Ordering};
+use branch_hints::unlikely;
 use crate::block::CacheLineAlign;
 
-pub const BLOCK_SIZE: usize = 4096;
+pub const BLOCK_SIZE: usize = if cfg!(miri) { 128 } else { 4096 };
 
 pub struct Block<T>{
     /// Set to None, when the first reader enters the next block.
@@ -14,23 +18,67 @@ pub struct Block<T>{
     // CacheLineAlign is CRUCIAL here for performance.
     pub read_counter : CacheLineAlign<AtomicUsize>,
 
-    mem: UnsafeCell<[MaybeUninit<T>; BLOCK_SIZE]>    
+    /// Freed as soon as read_counter == BLOCK_SIZE.
+    mem_ptr: NonNull<UnsafeCell<[MaybeUninit<T>; BLOCK_SIZE]>>,
 }
 
 impl<T> Default for Block<T>{
     fn default() -> Self {
+        let mem = Box::new(
+            UnsafeCell::new(
+                [const{ MaybeUninit::uninit() }; BLOCK_SIZE]
+            )        
+        );
         Self{
             next: Default::default(),
             write_counter: Default::default(),
             read_counter : Default::default(),
-            mem: UnsafeCell::new([const{ MaybeUninit::uninit() }; BLOCK_SIZE]),
+            mem_ptr: unsafe{ NonNull::new_unchecked(Box::into_raw(mem)) },
+        }
+    }
+}
+impl<T> Drop for Block<T>{
+    fn drop(&mut self) {
+        // Drop mem
+        let read_counter = self.read_counter.load(Ordering::Acquire);
+        let mem_deallocated = read_counter == BLOCK_SIZE;
+        // This could happen either in the very last block, 
+        // or if the whole queue was dropped.
+        if unlikely(!mem_deallocated) {
+            let mem = self.mem_ptr.as_ptr();
+            
+            // destruct remains
+            if mem::needs_drop::<T>(){
+                unsafe{
+                    let len = self.write_counter.load(Ordering::Acquire);
+                    let mem = (*mem).get_mut();
+                    for i in read_counter..len {
+                        ptr::drop_in_place(mem.get_unchecked_mut(i).assume_init_mut());
+                    }
+                }
+            }
+            
+            // dealloc
+            unsafe{
+                drop(Box::from_raw(mem));
+            }
         }
     }
 }
 
+
 impl<T> Block<T>{
+    /// Should be called ONCE.
+    /// All mem elements must be in destructed state.
     #[inline]
-    pub fn mem(&self) -> *const T {
-        self.mem.get().cast()
+    pub unsafe fn dealloc_destructed_mem(&self) {
+        debug_assert!(self.read_counter.load(Ordering::Acquire) == BLOCK_SIZE);
+        unsafe{ drop(Box::from_raw(self.mem_ptr.as_ptr())); }
+    }
+    
+    /// `mem` must exists.
+    #[inline]
+    pub unsafe fn mem_unchecked(&self) -> *const T {
+        self.mem_ptr.as_ref().get().cast()
     }
 }
--- src/unicast/spmc.rs
+++ src/unicast/spmc.rs
@@ -1,11 +1,9 @@
 //! Unbounded unicast spmc.
 
-use std::cell::UnsafeCell;
-use std::mem;
-use std::ops::Deref;
+use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 use std::sync::atomic::Ordering;
-use branch_hints::unlikely;
+use branch_hints::{likely, unlikely};
 use super::block::{BLOCK_SIZE, Block};
 
 #[derive(Default)]
@@ -46,7 +44,8 @@ impl<T> Queue<T> {
         }
         
         unsafe{
-            let mem = block.mem().cast_mut();
+            // TODO: cache mem pointer
+            let mem = block.mem_unchecked().cast_mut();
             mem.add(len).write(value);
         }
         
@@ -64,6 +63,56 @@ impl<T> Queue<T> {
     }
 }
 
+/// Owning [Queue] message wrapper.
+/// 
+/// # Design choices
+/// 
+/// We could just always return a value, but that would require mempcy for
+/// every read message. With ReadGuard we basically give you a reference, that 
+/// can be [take]n. 
+pub struct ReadGuard<'a, T>{
+    value: *mut T,
+    block_to_drop: Option<&'a Block<T>>
+}
+impl<'a, T> ReadGuard<'a, T>{
+    #[inline]
+    pub fn take(self) -> T {
+        unsafe{ std::ptr::read(self.value) }
+    }
+}
+impl<'a, T> Deref for ReadGuard<'a, T>{
+    type Target = T;
+
+    #[inline]
+    fn deref(&self) -> &Self::Target {
+        unsafe{ &*self.value }
+    }
+}
+impl<'a, T> DerefMut for ReadGuard<'a, T>{
+    #[inline]
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        unsafe{ &mut*self.value }
+    }
+}
+impl<'a, T> Drop for ReadGuard<'a, T>{
+    #[inline]
+    fn drop(&mut self) {
+        // 1. Drop value
+        unsafe {
+            std::ptr::drop_in_place(self.value);
+        }
+        
+        // 2. Drop block's mem, if needed.
+        if likely(self.block_to_drop.is_none()){
+            return;
+        }
+        unsafe{ 
+            self.block_to_drop.unwrap_unchecked()
+                .dealloc_destructed_mem(); 
+        }
+    }
+}
+
 pub struct Reader<T> {
     write_counter: usize,
     block: Arc<Block<T>>,
@@ -72,39 +121,39 @@ pub struct Reader<T> {
 unsafe impl<T> Send for Reader<T> {}
 
 impl<T: Default + Clone> Reader<T> {
-    // TODO: return wrapper that can take T, but returns &/&mut T by default.
     #[inline]
-    pub fn next(&mut self) -> Option<&T>{
+    pub fn next(&mut self) -> Option<ReadGuard<'_, T>>{
         let mut read_counter = self.block.read_counter.load(Ordering::Acquire);
         loop{
             if read_counter == self.write_counter {
                 if unlikely(read_counter == BLOCK_SIZE) {
-                    // It is highly unlikely that queue will stop EXACTLY at the last element of the block.
-                    // So it is OK just to lock spin::mutex every time we get here.
-                    //
-                    // As an alternative, we can cache read_block_ptr: AtopmicPtr in queue,
-                    // and has_next: AtomicBool in block for fast-path checks.
-                    let mut read_block = self.queue_shared_data.read_block.lock();
-                    if Arc::as_ptr(&read_block) != Arc::as_ptr(&self.block){
-                        // Use block from queue
-                        let read_block_ptr = read_block.clone();
-                        drop(read_block);
-                        self.block = read_block_ptr;
-                    } else {
-                        // try to get block from "next"
-                        let next_block = self.block.next.lock().take(); 
-                        if let Some(next_block) = next_block{
-                            // update read_block in queue
-                            *read_block = next_block.clone();
-                            drop(read_block);
-                            self.block = next_block;
+                    let next_block = {
+                        // It is highly unlikely that queue will stop EXACTLY at the last element of the block.
+                        // So it is OK just to lock spin::mutex every time we get here.
+                        //
+                        // As an alternative, we can cache read_block_ptr: AtopmicPtr in queue,
+                        // and has_next: AtomicBool in block for fast-path checks.                        
+                        let mut read_block = self.queue_shared_data.read_block.lock();
+                        if Arc::as_ptr(&read_block) != Arc::as_ptr(&self.block) {
+                            // Use last read block from the queue
+                            read_block.clone()
                         } else {
-                            // Have no more blocks
-                            return None;  
+                            // try to get block from "next"
+                            let next_block = self.block.next.lock().take(); 
+                            if let Some(next_block) = next_block{
+                                // update read_block in queue
+                                *read_block = next_block.clone();
+                                next_block
+                            } else {
+                                // Have no more blocks
+                                return None;  
+                            }
                         }
-                    }
-                    // just read everything again for new block.
-                    read_counter = self.block.read_counter.load(Ordering::Acquire);
+                    };
+                    // just read everything again for the new block.
+                    read_counter       = next_block.read_counter.load(Ordering::Acquire);
+                    self.write_counter = next_block.write_counter.load(Ordering::Acquire);
+                    self.block = next_block;
                     continue;
                 } else {
                     let write_counter = self.block.write_counter.load(Ordering::Acquire);
@@ -115,15 +164,27 @@ impl<T: Default + Clone> Reader<T> {
                 }
             }
 
-            // Can be weak as well - but looks like there is no difference.
             let result = self.block.read_counter.compare_exchange(read_counter, read_counter+1, 
                 Ordering::AcqRel,
                 Ordering::Acquire);
             match result {
                 Ok(_) => {
                     let index = read_counter;
-                    let value = unsafe{ &*self.block.mem().add(index) };
-                    return Some(value);    
+                    // TODO: Cache mem pointer
+                    let value = unsafe{ self.block.mem_unchecked().cast_mut().add(index) };
+                    
+                    return Some(
+                        ReadGuard{
+                            value,
+                            block_to_drop: 
+                                // Only one thread can get here, and only once per block.
+                                if unlikely(index == BLOCK_SIZE-1) { 
+                                    Some(self.block.as_ref()) 
+                                } else {
+                                    None
+                                },
+                        }
+                    );    
                 }
                 Err(new_read_counter) => { read_counter = new_read_counter }
             };
@@ -141,7 +202,7 @@ mod test {
         let mut queue: Queue<usize> = Default::default();
         let mut reader = queue.reader();
         
-        const COUNT: usize = BLOCK_SIZE*2;
+        const COUNT: usize = BLOCK_SIZE*20;
         for i in 0..COUNT{
             queue.push(i);    
         }
@@ -154,4 +215,53 @@ mod test {
 
         assert_equal(vec, 0..COUNT);
     }
+    
+    
+    pub mod message {
+        use std::fmt;
+    
+        const LEN: usize = 4;
+    
+        #[derive(Default, Clone, Copy)]
+        pub struct Message(#[allow(dead_code)] [usize; LEN]);
+        
+        impl fmt::Debug for Message {
+            fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+                f.pad("Message")
+            }
+        }
+        
+        #[inline]
+        pub fn new(num: usize) -> Message {
+            Message([num; LEN])
+        }    
+    }    
+    
+    #[test]
+    fn spsc_test(){
+        let mut queue = Queue::default();
+        let mut reader = queue.reader();
+        const COUNT: usize = BLOCK_SIZE*2 + 31;
+        
+        let wt = std::thread::spawn(move || {
+            for i in 0..COUNT {
+                queue.push(/*message::new(i)*/String::from(format!("{i}")));
+            }
+        });
+    
+        let rt = std::thread::spawn(move || {
+            for _ in 0..COUNT {
+                loop{
+                    if let None = reader.next(){
+                        std::thread::yield_now();
+                    } else {
+                        break;
+                    }
+                }
+            }
+        });
+        
+        wt.join().unwrap();
+        rt.join().unwrap();
+    }    
 }
